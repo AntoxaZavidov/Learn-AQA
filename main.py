33aba4a4a6f2f897812654c954efe1d400655edf
Gadzhik <gkurban@outlook.com>
2022-08-12 15:27:08 +0200

    tetse setset

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import requests
 
 payload = {"name": "Terminator"}
 responce = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-print(responce.text)
+print(responce.text{"name": "Terminator"})
 
