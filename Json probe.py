82d58ec25bc54525c7ea023d498bd4cbda9e9382
Gadzhik <gkurban@outlook.com>
2022-08-18 10:36:31 +0200

    tetse setset

--- Json probe.py
+++ Json probe.py
@@ -1,13 +1,12 @@
 import json
 import requests
 
-responce = requests.get("https://qa07.happify.com/api/happifiers/4578/")
+responce = requests.get("https://stage.happify.com/api/happifiers/topics/")
 print(responce.text)
 
 string_as_json_format = responce.text
 obj = json.loads(string_as_json_format)
 
 print(obj['human_url'])
-print(obj['modified_at'])
-print(obj['sponsor'])
+
 
--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import requests
 
 payload = {"name": "Terminator"}
 responce = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-print(responce.text{"name": "Terminator"})
+print(responce.text)
+
 
