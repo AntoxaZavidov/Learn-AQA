63fcb228e86caf57808d8fe992fe11eec6496fa8
Gadzhik <gkurban@outlook.com>
2022-08-31 15:28:59 +0200

    response cookies

--- Stepic.py
+++ Stepic.py
@@ -1,11 +1,8 @@
-n = int(input())
-f1 = 0
-f2 = 1
+import requests
 
-for i in range(1, n+1):
+payload = {"email":"zavidov2@alarstudios.com", "password": "Password_1"}
+response = requests.get("https://qa02.happify.com/admin/", params=payload)
 
 
-    f1,f2=f2,f1 + f2
-
-    print(f1, end=' ')
-
+print(response.status_code)
+print(dict(response.cookies))
