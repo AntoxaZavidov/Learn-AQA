04b66fc2dba00a9b968f362c16df908937badec2
Gadzhik <gkurban@outlook.com>
2022-09-14 11:48:13 +0200

    First positive test

--- L22_PositiveTest.py
+++ L22_PositiveTest.py
@@ -1,6 +1,6 @@
 import requests
 
-class TetsUserAuth:
+class TestUserAuth:
     def test_auth_user(self):
 
         data = {
@@ -8,26 +8,26 @@ class TetsUserAuth:
             'password': '1234'
         }
 
-        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data = data)
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
         assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
-        assert "x_csrf_token" in response1.headers, "There is no CSRF token header in the response"
+        assert "x-csrf-token" in response1.headers, "There is no CSRF token header in the response"
         assert "user_id" in response1.json(), "There is no user id in the response"
 
-        auth_sid  = response1.cookies.get("auth_sid")
-        token = response1.headers.get("x_csrf_token")
+        auth_sid = response1.cookies.get("auth_sid")
+        token = response1.headers.get("x-csrf-token")
         user_id_from_auth_method = response1.json()["user_id"]
 
         response2 = requests.get(
             "https://playground.learnqa.ru/api/user/auth",
-            headers={"x_csrf_token":token},
+            headers={"x-csrf-token":token},
             cookies={"auth_sid":auth_sid}
 
         )
 
         assert "user_id" in response2.json(), "There is no user id in the second response"
-        user_id_from_auth_method = response2.json()["user_id"]
+        user_id_from_check_method = response2.json()["user_id"]
 
-        assert  user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal to user id from check method"
+        assert user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal to user id from check method"
 
 
 
